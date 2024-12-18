f1562321229792d99148f8d524ff11b18b970ba2
rimasultana <rimasultana20225@gmail.com>
2024-12-19 00:23:59 +0600

    animation done

--- src/pages/Banner.jsx
+++ src/pages/Banner.jsx
@@ -1,4 +1,6 @@
 import { easeOut, motion } from "framer-motion";
+import team1 from "../assets/team/team1.jpg";
+import team2 from "../assets/team/team2.jpg";
 
 const Banner = () => {
   return (
@@ -6,9 +8,17 @@ const Banner = () => {
       <div className="hero bg-base-200 min-h-screen">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <div className="flex-1">
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-              className="max-w-sm rounded-lg shadow-2xl"
+            <motion.img
+            animate={{ y: [50, 100, 50]}}
+            transition={{duration: 5, repeat: Infinity }}
+              src={team1}
+              className="max-w-sm w-64 rounded-tl-[40px]  border-l-4 border-b-4 border-blue-700 shadow-2xl"
+            />
+            <motion.img
+            animate={{ x: [100, 150, 100]}}
+            transition={{duration: 5, repeat: Infinity }}
+              src={team2}
+              className="max-w-sm w-64 rounded-tl-[40px]  border-l-4 border-b-4 border-blue-700 shadow-2xl"
             />
           </div>
           <div>
@@ -22,12 +32,19 @@ const Banner = () => {
               }}
               className="text-5xl font-bold"
             >
-              Latest Jobs For you!
+              Latest{" "}
+              <motion.span
+                animate={{ color: ["#5b33ff", "#e933ff"] }}
+                transition={{ duration: 1.5, repeat: Infinity, delay: 1 }}
+              >
+                Jobs
+              </motion.span>{" "}
+              For you!
             </motion.h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+            <p className="py-8">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut <br /> assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut 
+              repudiandae et a id nisi.
             </p>
             <button className="btn btn-primary">Get Started</button>
           </div>
