28a0549b1fe5624f33c9238c498c2cddd4748e5c
rimasultana <rimasultana20225@gmail.com>
2024-12-31 23:36:43 +0600

    card done

--- src/pages/HotJobsCard.jsx
+++ src/pages/HotJobsCard.jsx
@@ -1,3 +1,5 @@
+import { p } from "motion/react-client";
+import { IoLocation } from "react-icons/io5";
 
 const HotJobsCard = ({ job }) => {
   const {
@@ -15,16 +17,33 @@ const HotJobsCard = ({ job }) => {
   } = job;
   return (
     <div>
-      <div className="card card-compact bg-base-100 shadow-xl">
-        <figure>
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-            alt="Shoes"
-          />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
+      <div className="card card-compact w-[300px] h-[400px] bg-base-100 shadow-xl">
+        <div className="flex gap-2 m-2">
+          <figure>
+            <img className="w-16 object-cover" src={company_logo} alt={title} />
+          </figure>
+          <div>
+            <h4>{company}</h4>
+            <h2 className="flex items-center">
+              <span>
+                <IoLocation />
+              </span>
+              <span>{location}</span>
+            </h2>
+          </div>
+        </div>
+        <div className="card-body overflow-auto">
+          <h2 className="card-title">
+            {title}
+            <div className="badge badge-secondary">NEW</div>
+          </h2>
+
+          <p>{description}</p>
+          <div className="flex gap-2 flex-wrap">
+            {
+                requirements.map(skill => <p className="border rounded-md py-2 text-center hover:bg-red-200 hover:text-green-500">{skill}</p>)
+            }
+          </div>
           <div className="card-actions justify-end">
             <button className="btn btn-primary">Buy Now</button>
           </div>
